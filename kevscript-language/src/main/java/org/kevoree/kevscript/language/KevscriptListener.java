82a081825b2a4a270f83ec656ed6baa7ea01a16e
Manuel Leduc <manuel.leduc@inria.fr>
2016-03-01 16:20:13 +0100

    wip

--- kevscript-language/src/main/java/org/kevoree/kevscript/language/KevscriptListener.java
+++ kevscript-language/src/main/java/org/kevoree/kevscript/language/KevscriptListener.java
@@ -1,7 +1,6 @@
 package org.kevoree.kevscript.language;
 
 import org.kevoree.kevscript.KevScriptBaseListener;
-import org.kevoree.kevscript.KevScriptParser;
 
 import static org.kevoree.kevscript.KevScriptParser.*;
 
@@ -12,6 +11,6 @@ public class KevscriptListener extends KevScriptBaseListener {
     @Override
     public void enterAdd(AddContext ctx) {
         super.enterAdd(ctx);
-        Left_add_definitionsContext lstMembers = ctx.list_add_members;
+        //System.out.println("add " + ctx.list_add_members.getText() +":"+ctx.typeDef.getText());
     }
 }
--- kevscript-language/src/main/java/org/kevoree/kevscript/language/Tmp.java
+++ kevscript-language/src/main/java/org/kevoree/kevscript/language/Tmp.java
@@ -13,7 +13,8 @@ import static org.kevoree.kevscript.KevScriptParser.*;
  */
 public class Tmp {
     public static void main(String args[]) {
-        final String input = "add node0: JavaNode";
+        final String input = "add node0: JavaNode\n" +
+                "add node1, node2: JavaNode/1 {}";
         final KevScriptLexer lexer = new KevScriptLexer(new ANTLRInputStream(input));
         final CommonTokenStream tokens = new CommonTokenStream(lexer);
         final KevScriptParser parser = new KevScriptParser(tokens);
